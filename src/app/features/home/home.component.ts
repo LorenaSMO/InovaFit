e8bb13d6fbb839f992c02b9d11d6007889e6b5ee
vasconcelos-gabriel <vasconcelos.crvg@gmail.com>
2024-10-06 00:31:34 -0300

    feat: home implementada

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../shared/components/button/button.component";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
