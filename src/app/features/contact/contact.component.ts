48b81cdb7f9e58177d68cf175cffb3e852dc77be
vasconcelos-gabriel <vasconcelos.crvg@gmail.com>
2024-10-08 18:44:59 -0300

    chore: manutencao do componente pricing

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
